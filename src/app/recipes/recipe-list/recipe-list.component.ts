fec2bc0e0f9489bb749605460cf0186b0a799983
Matthew Wetter <mwetter@campuslabs.com>
2019-04-25 14:41:00 -0400

    add more detail to receipes and shopping list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,7 +9,7 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe('Test Recipe', 'this is a test', 'https://www.gannett-cdn.com/-mm-/1fecae5856e58374cc9e1c0fd6dcc3c6aae79d4e/c=0-293-5760-3547/local/-/media/2018/07/17/IAGroup/DesMoines/636674359927753055-0717-NEW-STATEFAIR-FOODS-00029.jpg?width=3200&height=1680&fit=crop'),
-    new Recipe('Test Recipe', 'this is a test', 'https://www.gannett-cdn.com/-mm-/1fecae5856e58374cc9e1c0fd6dcc3c6aae79d4e/c=0-293-5760-3547/local/-/media/2018/07/17/IAGroup/DesMoines/636674359927753055-0717-NEW-STATEFAIR-FOODS-00029.jpg?width=3200&height=1680&fit=crop')
+    new Recipe('Test Recipe', 'this is a test', 'https://cdn.vox-cdn.com/thumbor/gcLdEduI29mEc9RhG7_l9GDaZb4=/0x0:844x844/1200x800/filters:focal(355x355:489x489)/cdn.vox-cdn.com/uploads/chorus_image/image/54896147/bud_and_marilyns.0.jpg')
   ];
 
   constructor() { }
