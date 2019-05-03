82fc270848018d7cd577697cd70651ad34c7dc9f
Matthew Wetter <mwetter@campuslabs.com>
2019-05-03 08:16:47 -0400

    use service instead and update package dependency

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,19 +8,12 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
-  recipes: Recipe[] = [
-    new Recipe('Test Recipe', 'this is a test', 'https://www.gannett-cdn.com/-mm-/1fecae5856e58374cc9e1c0fd6dcc3c6aae79d4e/c=0-293-5760-3547/local/-/media/2018/07/17/IAGroup/DesMoines/636674359927753055-0717-NEW-STATEFAIR-FOODS-00029.jpg?width=3200&height=1680&fit=crop'),
-    new Recipe('Another Test Recipe', 'this is another test', 'https://cdn.vox-cdn.com/thumbor/gcLdEduI29mEc9RhG7_l9GDaZb4=/0x0:844x844/1200x800/filters:focal(355x355:489x489)/cdn.vox-cdn.com/uploads/chorus_image/image/54896147/bud_and_marilyns.0.jpg')
-  ];
+  recipes: Recipe[];
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
-  }
-
-  onRecipeSelected(recipe: Recipe) {
-    this.recipeWasSelected.emit(recipe);
+    this.recipes = this.recipeService.getRecipes();
   }
 
 }
---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,15 @@
+import { EventEmitter } from "@angular/core";
+import { Recipe } from "./recipe.model";
+
+export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
+  private recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'this is a test', 'https://www.gannett-cdn.com/-mm-/1fecae5856e58374cc9e1c0fd6dcc3c6aae79d4e/c=0-293-5760-3547/local/-/media/2018/07/17/IAGroup/DesMoines/636674359927753055-0717-NEW-STATEFAIR-FOODS-00029.jpg?width=3200&height=1680&fit=crop'),
+    new Recipe('Another Test Recipe', 'this is another test', 'https://cdn.vox-cdn.com/thumbor/gcLdEduI29mEc9RhG7_l9GDaZb4=/0x0:844x844/1200x800/filters:focal(355x355:489x489)/cdn.vox-cdn.com/uploads/chorus_image/image/54896147/bud_and_marilyns.0.jpg')
+  ];
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+}
